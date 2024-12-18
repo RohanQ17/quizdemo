7f237ef0ba38fe614bf3fe932441a17e3db5f0cf
RohanQ17 <axelb4467@gmail.com>
2024-12-18 16:36:59 +0530

    changed settings.py

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -21,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ug36xqo1@&j&(57=5^_ta##taj!pas#@dpcb-^_^l8g$yby=8^'
+
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS =['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
