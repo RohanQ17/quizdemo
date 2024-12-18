05842acb4d9be0a1f37978c413dabe97036b77d6
RohanQ17 <axelb4467@gmail.com>
2024-12-18 17:48:07 +0530

    phew more fixes

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app']
 
 # Application definition
 
